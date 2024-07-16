8a2756d03213b89e08d8b09c08a57722546982eb
br0wnD3v <bothereum@protonmail.com>
2024-07-16 08:56:42 +0530

    Basic Mobile View.

--- pages/components/home/Hero.js
+++ pages/components/home/Hero.js
@@ -1,33 +1,48 @@
-import { Flex, Spacer, Image, Heading, Text } from "@chakra-ui/react";
+import { Flex, Image, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import "@fontsource/turret-road/800.css";
 
 export default function Hero() {
+  const [isLargerThanMd] = useMediaQuery("(min-width: 1280px)");
+
   return (
     <>
       <Flex position={"relative"} w="100%" h="100%">
         <Image
-          position={"absolute"}
           src="background.png"
+          position={"absolute"}
           zIndex={-1}
+          filter={!isLargerThanMd ? "brightness(50%)" : "none"}
           bgSize={"cover"}
           bgRepeat={"no-repeat"}
-          w={"100%"}
+          w={isLargerThanMd ? "100%" : ""}
+          h={"100vh"}
           alt="bg"
         />
         <Flex
           direction={"column"}
-          w={"35%"}
-          h="90vh"
-          p={5}
-          gap={2}
+          h="70vh"
+          w={isLargerThanMd ? "38%" : "100%"}
+          p={isLargerThanMd ? 10 : 30}
+          gap={3}
           position={"absolute"}
+          justify={"center"}
           right={0}
           pt={"12%"}
         >
-          <Heading color={"#FF006B"} fontFamily={"Turret Road"} size={"2xl"}>
+          <Heading
+            color={"#FF006B"}
+            fontFamily={"Turret Road"}
+            size={"3xl"}
+            textAlign={!isLargerThanMd ? "center" : "initial"}
+          >
             You Are Not Bullish Enough Anon
           </Heading>
-          <Text color="white" fontFamily={"Turret Road"} fontSize={"40px"}>
+          <Text
+            color="white"
+            fontFamily={"Turret Road"}
+            fontSize={"36px"}
+            textAlign={!isLargerThanMd ? "center" : "initial"}
+          >
             Coming Soon...
           </Text>
         </Flex>
